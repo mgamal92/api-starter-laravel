6590c33a4990770cff1f8e8acb0f3d3724df23d7
Mohamed Gamal <mg.dev1992@gmail.com>
2020-02-13 19:58:03 +0200

    add service provider

---
+++ src/BaristaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Barista;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaristaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
